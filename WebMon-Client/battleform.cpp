5a85831f575cd0634c526bed02a555a5d7a7a9db
David Sussman <davsuss@vt.edu>
2013-11-14 15:29:51 -0500

    Made progress bar headway

--- WebMon-Client/battleform.cpp
+++ WebMon-Client/battleform.cpp
@@ -1,6 +1,9 @@
 #include "battleform.h"
 #include "ui_battleform.h"
 
+QString progress = " QProgressBar {border: 1px solid black;border-bottom-right-radius: 9px;}  QProgressBar::chunk {background-color: #33CC00;width: 20px;}";
+
+
 BattleForm::BattleForm(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::BattleForm)
@@ -8,6 +11,7 @@ BattleForm::BattleForm(QWidget *parent) :
     ui->setupUi(this);
     QPixmap map;
     ui->Background->lower();
+    ui->progressBar->setStyleSheet(progress);
     setGif("004.gif",ui->FriendlySprite);
     setGif("004-back.gif",ui->EnemySprite);
 }
